813d1a038b6480fb11a5cdb1aab8e5542635d2cf
Nicky Semenza <14nicholasse@gmail.com>
2015-10-12 00:51:02 -0400

    scanner blank v1

---
+++ www/js/controllers.js
@@ -0,0 +1,43 @@
+angular.module('starter.controllers', ['ngCordova'])
+
+.controller('DashCtrl', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPlatform) {
+
+  $scope.scanBarcode = function() {
+        console.log('a');
+        $scope.code = "waiting";
+        $cordovaBarcodeScanner.scan().then(function(imageData) {
+            // alert(imageData.text);
+            $scope.code=imageData.text;
+            console.log("Barcode Format -> " + imageData.format);
+            console.log("Cancelled -> " + imageData.cancelled);
+        }, function(error) {
+            console.log("An error happened -> " + error);
+        });
+    };
+
+})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  };
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+});
