b8b440220ab9f135388f9fcdf8d1d15b5828a62b
Nicky Semenza <14nicholasse@gmail.com>
2015-10-13 01:32:32 -0400

    99% functionality

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,19 +1,59 @@
-angular.module('starter.controllers', ['ngCordova'])
+angular.module('starter.controllers', ['ngCordova','restangular'])
 
-.controller('DashCtrl', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPlatform) {
+.controller('DashCtrl', function($scope, $rootScope, $cordovaBarcodeScanner, $ionicPlatform, Restangular) {
+  $scope.show_result=false;
+  $scope.last_checked_in = "n/a";
+  var clearForms = function()
+  {
+    $scope.input_email = "";
+    $scope.show_result = false;
+  }
 
   $scope.scanBarcode = function() {
-        console.log('a');
-        $scope.code = "waiting";
+    clearForms();
+        $scope.input_email = "waiting";
         $cordovaBarcodeScanner.scan().then(function(imageData) {
             // alert(imageData.text);
-            $scope.code=imageData.text;
+            $scope.input_email=imageData.text;
             console.log("Barcode Format -> " + imageData.format);
             console.log("Cancelled -> " + imageData.cancelled);
         }, function(error) {
             console.log("An error happened -> " + error);
         });
     };
+
+  $scope.processEmail = function() {
+    Restangular.setBaseUrl("http://localhost:3000");
+    Restangular.one('/execs/hacker_checkin_info')
+               .get({'checkin_token': '9kvKkdcDyOxvA9aZ35QJJw','email':$scope.input_email})
+               .then(function (response) {
+                    $scope.show_result=true;
+                    console.log(response);
+                    $scope.hacker = response;
+                    if(response.status=="not_found")
+                    {
+                      $scope.bad_result=true;
+                    }
+                    else
+                    {
+                      $scope.bad_result=false;
+                    }
+
+                }
+            );
+  }; 
+  $scope.checkIn = function() {
+    Restangular.setBaseUrl("http://localhost:3000");
+    console.log($scope.input_email);
+    Restangular.all('/execs/checkin')
+               .customPOST({'checkin_token': '9kvKkdcDyOxvA9aZ35QJJw','email':$scope.input_email})
+               .then(function(data) {
+                console.log(data);
+                $scope.last_checked_in = $scope.hacker.name;
+                clearForms();
+               });
+
+  };
 
 })
 
